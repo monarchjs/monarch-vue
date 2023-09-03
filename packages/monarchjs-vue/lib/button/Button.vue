6ed495c5f951eb229c53f49cb93232edb5ec58f6
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2023-09-03 13:26:00 -0400

    first commit

---
+++ packages/monarchjs-vue/lib/button/Button.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+export interface Props {
+  label?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  label: 'hello',
+})
+
+const { label } = reactive(props)
+</script>
+
+<template>
+  <button>
+    {{ label }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: 0.6rem 1.25rem;
+  background-color: #747bff;
+  border: 0;
+}
+button:hover {
+  background-color: #535bf2;
+}
+</style>
---
+++ packages/monarchjs-vue/vite.config.ts
@@ -0,0 +1,48 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: 'lib/index.ts',
+      name: 'monarch',
+      fileName: (format) => `monarch.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that should not be bundled
+      // into your library
+      input: {
+        main: resolve(__dirname, 'lib/index.ts'),
+      },
+      external: ['vue'],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'main.css') return 'monarch.css'
+          return assetInfo.name
+        },
+        exports: 'named',
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      ui: resolve(__dirname, 'lib'),
+    },
+  },
+})
